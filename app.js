3c6b87e109dcdb8529c98559fa4864957bc3170d
DarknesS-SHA <sf.fiore18@gmail.com>
2025-09-21 13:11:18 -0500

    feat: agregar función para añadir amigos a la lista

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo(nombre) {
+    let nombreAmigo = document.getElementById("amigo").value
+    amigos.push(nombreAmigo)
+
+    let lista = document.getElementById("listaAmigos")
+    let li = document.createElement("li")
+    li.textContent = nombreAmigo
+    lista.appendChild(li)
+
+    document.getElementById("amigo").value = ""
+
+}
